305254b6dfe30cb5df01f7f26b5020134f810e6d
Clay Gunnels <ctg62115@uga.edu>
2021-12-13 16:29:42 -0500

    commit

--- storefront/settings.py
+++ storefront/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-u&j-dpwxp2y_hj0e*2s7$gi9!-!qd-mw$mo3xv4kvjq8h!)q)y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
