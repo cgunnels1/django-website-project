3cc3670fdaf8c0934107fdf7489b5c44f9577284
Clay Gunnels <ctg62115@uga.edu>
2022-02-06 17:45:27 -0500

    Edit html page and models

--- playlist/models.py
+++ playlist/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Song(models.Model):
-    name = models.CharField(max_length=60)
+    title = models.CharField(max_length=60)
     writer = models.CharField(max_length=60)
+    
+    def __str__(self):
+        return self.title
